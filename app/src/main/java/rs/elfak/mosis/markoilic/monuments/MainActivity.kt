e3e7bbfaef1fa7e703d13f96b51f00fc5d910e50
MarkoIlic <markoilic@webelinx.com>
2022-08-14 21:19:19 +0200

    Added activity_main layout

--- app/src/main/java/rs/elfak/mosis/markoilic/monuments/MainActivity.kt
+++ app/src/main/java/rs/elfak/mosis/markoilic/monuments/MainActivity.kt
@@ -2,10 +2,17 @@ package rs.elfak.mosis.markoilic.monuments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import rs.elfak.mosis.markoilic.monuments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/rs/elfak/mosis/markoilic/monuments/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package rs.elfak.mosis.markoilic.monuments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
