cb33014aa8c10195c1623bae019e1a4a9672c4b9
MarkoIlic <markoilic@webelinx.com>
2022-08-20 16:12:37 +0200

    Register Activity

--- app/src/main/java/rs/elfak/mosis/markoilic/monuments/RegisterActivity.kt
+++ app/src/main/java/rs/elfak/mosis/markoilic/monuments/RegisterActivity.kt
@@ -2,10 +2,17 @@ package rs.elfak.mosis.markoilic.monuments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import rs.elfak.mosis.markoilic.monuments.databinding.ActivityLoginBinding
+import rs.elfak.mosis.markoilic.monuments.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
     }
 }
